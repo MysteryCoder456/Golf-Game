5c0c94bb45ea991d807e4d521c3bf70c838bec85
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-08-03 10:34:44 +0400

    added some exception handling and fixed errors

--- game/level.py
+++ game/level.py
@@ -7,7 +7,7 @@ class Level:
     def __init__(self, win_size: vec2, tile_size: int = 34, tile_map=None):
         self.tile_size = int(tile_size)
 
-        while win_size.x % self.tile_size != 0 and win_size.y % self.tile_size != 0:
+        while not (win_size.x % self.tile_size == 0 and win_size.y % self.tile_size == 0):
             self.tile_size -= 1
 
         self.cols = int(win_size.x / self.tile_size)
@@ -22,16 +22,16 @@ class Level:
                 "000000000000000000000000000000",
                 "000000000000000000000000000000",
                 "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
                 "000000000011111111110000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
+                "000000000011111111110000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
+                "000000000000001100000000000000",
                 "000000000000000000000000000000",
                 "000000000000000000000000000000",
                 "000000000000000000000000000000",
@@ -44,13 +44,17 @@ class Level:
     def build_level(self):
         tiles = []
         tile_map = self.tile_map
-        for y in range(self.rows):
-            for x in range(self.cols):
-                if tile_map[y][x] == "1":
-                    tile_x = x * self.tile_size
-                    tile_y = y * self.tile_size
-                    tile = Tile(vec2(tile_x, tile_y), vec2(self.tile_size))
-                    tiles.append(tile)
+        try:
+            for y in range(self.rows):
+                for x in range(self.cols):
+                    if tile_map[y][x] == "1":
+                        tile_x = x * self.tile_size
+                        tile_y = y * self.tile_size
+                        tile = Tile(vec2(tile_x, tile_y), vec2(self.tile_size))
+                        tiles.append(tile)
+        except IndexError:
+            raise Exception("Provided tilemap size is bigger than expected")
+
         self.tiles = tiles
 
     def render(self, win):
